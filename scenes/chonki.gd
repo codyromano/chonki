ef297cc923fb671f1c278e45e4ff7d57096c6ff5
Cody Romano <codyromano@gmail.com>
2025-06-17 22:45:16 -0700

    add ground

--- scenes/chonki.gd
+++ scenes/chonki.gd
@@ -1,16 +1,42 @@
 extends Node2D
 
-@onready var sprite      : AnimatedSprite2D     = $CharacterBody2D/AnimatedSprite2D
-@onready var run_sound   : AudioStreamPlayer2D  = $AudioRun
-@onready var rest_sound  : AudioStreamPlayer2D  = $RestRun
+@onready var body         : CharacterBody2D     = $CharacterBody2D
+@onready var sprite       : AnimatedSprite2D     = $CharacterBody2D/AnimatedSprite2D
+@onready var run_sound    : AudioStreamPlayer2D  = $AudioRun
+@onready var rest_sound   : AudioStreamPlayer2D  = $RestRun
+@onready var ram_sound    : AudioStreamPlayer2D  = $AudioRam
+@onready var push_sound   : AudioStreamPlayer2D  = $AudioPush
 
 enum ChonkiState { IDLE, RUN, ATTACK }
 
-var state : ChonkiState  = ChonkiState.IDLE
+var state : ChonkiState = ChonkiState.IDLE
 var last_action_time : float = Time.get_unix_time_from_system()
 
+var velocity: Vector2 = Vector2.ZERO
+const SPEED: float = 2000.0
+const JUMP_FORCE: float = -2500.0
+const GRAVITY: float = 3000.0
+
+		
+func _physics_process(delta: float) -> void:	
+	handle_movement(delta)
+	update_sprite()
+
+func handle_movement(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+
+	if Input.is_action_just_pressed("ui_up") and body.is_on_floor():
+		$AudioJump.play()
+		velocity.y = JUMP_FORCE
+
+	var direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.x = direction * SPEED
+
+	body.velocity = velocity
+	body.move_and_slide()
+
 func play_once(player: AudioStreamPlayer2D) -> void:
-	if !player.playing:
+	if not player.playing:
 		player.play()
 
 func play_sound_effect() -> void:
@@ -19,14 +45,16 @@ func play_sound_effect() -> void:
 	var secs_since_action = Time.get_unix_time_from_system() - last_action_time
 
 	if anim == "ram":
-		play_once($AudioRam)
+		play_once(ram_sound)
 	elif anim == "push":
-		play_once($AudioPush)
+		play_once(push_sound)
 
-	if anim in ["ram", "push"]:
+	# Stop both sounds during attack or jump
+	if anim in ["ram", "push", "jump"]:
 		run_sound.stop()
 		rest_sound.stop()
 	elif is_running:
+		# Only play run sound if NOT jumping, ramming, or pushing
 		if not run_sound.playing:
 			run_sound.play()
 		rest_sound.stop()
@@ -38,27 +66,24 @@ func play_sound_effect() -> void:
 		run_sound.stop()
 		rest_sound.stop()
 
-func _process(_delta: float) -> void:
-	update_sprite()
-
 func update_sprite() -> void:
 	var is_taking_action: bool = false
 	var current_animation = sprite.animation
-	
+
 	if current_animation in ["ram", "push"] and sprite.is_playing():
 		return
-	
+
 	if Input.is_action_just_pressed("push"):
 		sprite.play("push")
 		is_taking_action = true
 	elif Input.is_action_just_pressed("ram"):
 		sprite.play("ram")
 		is_taking_action = true
-	elif Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left"):
+	elif velocity.x != 0:
 		sprite.play("run")
 		is_taking_action = true
 
-	if Input.is_action_pressed("ui_up"):
+	if not body.is_on_floor():
 		sprite.play("jump")
 		is_taking_action = true
 
@@ -79,4 +104,3 @@ func update_sprite() -> void:
 		sprite.play("idle")
 
 	play_sound_effect()
-
