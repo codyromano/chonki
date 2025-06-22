4cb3dbbacad26b78cb094e60685ba19525f311e7
Cody Romano <codyromano@gmail.com>
2025-06-21 19:48:48 -0700

    stop timer on game win

--- scenes/menus/TimerText.gd
+++ scenes/menus/TimerText.gd
@@ -5,11 +5,15 @@ extends Label
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	text = str(duration)
+	GlobalSignals.win_game.connect(stop_timer)
+
+func stop_timer() -> void:
+	$Timer.queue_free()
 
 func _on_timer_timeout():
 	if duration == 0:
 		GlobalSignals.time_up.emit()
-		$Timer.queue_free()
+		stop_timer()
 		
 	duration = max(0, duration - 1)
 		
