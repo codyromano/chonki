0bdf8aa348867e91acee9576a75efed977e5181e
Cody Romano <codyromano@gmail.com>
2025-07-16 21:19:20 -0700

    add bicylist NPC

--- scenes/composable/bicyclist_patrol_loop.gd
+++ scenes/composable/bicyclist_patrol_loop.gd
@@ -3,4 +3,4 @@ extends "res://scenes/NPCLinearPatrolLoop.gd"
 func _on_change_direction(moving_toward_end: bool, _sprite: AnimatedSprite2D) -> void:
 	# Flip the bicyclist when she's returning to the start marker
 	_sprite.flip_h = !moving_toward_end 
-	_sprite.rotation_degrees = -_sprite.rotation_degrees
+	#_sprite.rotation_degrees = -_sprite.rotation_degrees
