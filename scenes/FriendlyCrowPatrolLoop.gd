82499fa10db7d0c8e59651847057d485d3371987
Cody Romano <codyromano@gmail.com>
2025-07-19 14:18:57 -0700

    fix branch logic

--- scenes/FriendlyCrowPatrolLoop.gd
+++ scenes/FriendlyCrowPatrolLoop.gd
@@ -1,13 +1,13 @@
 extends "res://scenes/NPCLinearPatrolLoop.gd"
 
 func _on_change_direction(moving_toward_end: bool, crow_sprite: AnimatedSprite2D) -> void:
-	var branch: Area2D = crow_sprite.get_parent().find_child("Branch")
-	var branch_sprite: Sprite2D = branch.find_child("Sprite2D")
+	var branch: Sprite2D = crow_sprite.get_parent().find_child("Branch")
+	# var branch_sprite: Sprite2D = branch.find_child("Sprite2D")
 
 	# Flip the crow when returning to the start marker
 	crow_sprite.flip_h = moving_toward_end
 	crow_sprite.rotation_degrees = -crow_sprite.rotation_degrees
 
 	branch.position = -branch.position
-	branch_sprite.flip_h = !branch_sprite.flip_h
-	branch_sprite.rotation_degrees = -branch_sprite.rotation_degrees
+	branch.flip_h = !branch.flip_h
+	branch.rotation_degrees = -branch.rotation_degrees
