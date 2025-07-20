2f7e6c943a785d2cccdf448a605544a3a52f3fc9
Cody Romano <codyromano@gmail.com>
2025-07-19 19:36:40 -0700

    respawning branches

--- scenes/FriendlyCrowPatrolLoop.gd
+++ scenes/FriendlyCrowPatrolLoop.gd
@@ -1,9 +1,43 @@
 extends "res://scenes/NPCLinearPatrolLoop.gd"
 
+@export var branch_scene: PackedScene
+@export var branch_initial_position: Vector2
+@export var branch_initial_rotation: float
+
+var current_branch: Node = null
+
+func _ready():
+	super()
+	# Instantiate the first branch
+	_respawn_branch()
+	GlobalSignals.biker_cleaned_up_branch.connect(_on_biker_cleaned_up_branch)
+
+func _on_biker_cleaned_up_branch():
+	_respawn_branch()
+
+func _respawn_branch():
+	if branch_scene:
+		# Clean up the old branch if it exists
+		if is_instance_valid(current_branch):
+			current_branch.queue_free()
+
+		var new_branch = branch_scene.instantiate()
+		body.add_child(new_branch)
+		new_branch.position = branch_initial_position
+		new_branch.rotation = branch_initial_rotation
+		current_branch = new_branch
+
 func _on_change_direction(_moving_toward_end: bool, crow_sprite: AnimatedSprite2D) -> void:
 	# Flip the crow when returning to the start marker
 	crow_sprite.flip_h = !crow_sprite.flip_h
 
-	var branch_body: CharacterBody2D = crow_sprite.get_parent().find_child("Branch")
-	if branch_body == null:
+	if !is_instance_valid(current_branch):
 		return
+	
+	# Flip the branch's position based on the crow's direction
+	if crow_sprite.flip_h:
+		current_branch.position.x = -branch_initial_position.x
+		current_branch.rotation = -branch_initial_rotation
+	else:
+		current_branch.position.x = branch_initial_position.x
+		current_branch.rotation = branch_initial_rotation
