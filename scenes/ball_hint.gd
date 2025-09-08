8f31baf906ec6421fe822e7f920de83d8a870e42
Cody Romano <codyromano@gmail.com>
2025-09-07 21:02:01 -0700

    more ux improvements

--- scenes/ball_hint.gd
+++ scenes/ball_hint.gd
@@ -5,5 +5,5 @@ func _ready() -> void:
 
 func _on_item_collected(_collectible_name: String) -> void:
 	GlobalSignals.queue_main_dialogue.emit(
-		"Hint: I don't have enough time to pull the lever by myself. Maybe one  of my siblings can help"
+		"Hint: I don't have enough time to pull the lever by myself. One of my siblings might help if I can find a way to wake them up."
 	)
