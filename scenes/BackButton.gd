e7ce3fac3c92928dafb83d50bd8b1c3faede22e4
Cody Romano <codyromano@gmail.com>
2025-09-12 22:55:58 -0700

    xbox joystick mapping

--- scenes/BackButton.gd
+++ scenes/BackButton.gd
@@ -1,9 +1,38 @@
 extends Button
 
+func _ready() -> void:
+	# Set up focus navigation to letter buttons when pressing up
+	call_deferred("_setup_focus_navigation")
+
+func _setup_focus_navigation() -> void:
+	# Find the SelectLettersButtonContainer specifically
+	var scene_root = get_tree().current_scene
+	
+	var letter_container = scene_root.find_child("SelectLettersButtonContainer", true, false)
+	
+	if letter_container:
+		var first_letter_button = null
+		
+		# Find the first focusable letter button
+		for child in letter_container.get_children():
+			if child is Button and child.focus_mode != Control.FOCUS_NONE and not child.disabled:
+				if not first_letter_button:
+					first_letter_button = child
+					set("focus_neighbor_up", child.get_path())
+		
+		# Set up down neighbors for all letter buttons
+		if first_letter_button:
+			_setup_letter_button_down_neighbors(letter_container)
+
+func _setup_letter_button_down_neighbors(letter_container: Node) -> void:
+	# Set this back button as the down neighbor for all letter buttons
+	for child in letter_container.get_children():
+		if child is Button and child.focus_mode != Control.FOCUS_NONE:
+			child.set("focus_neighbor_down", get_path())
+
 func _gui_input(event: InputEvent) -> void:
 	# Handle both "read" and "jump" actions to activate the back button
 	if has_focus() and (event.is_action_pressed("read") or event.is_action_pressed("jump")):
-		print("BackButton handling action - has focus: ", has_focus())
 		_on_pressed()
 		var viewport = get_viewport()
 		if viewport:
--- scenes/ResetButton.gd
+++ scenes/ResetButton.gd
@@ -9,6 +9,22 @@ func _ready() -> void:
 	GlobalSignals.press_reset_anagram.connect(
 		_press_reset_anagram
 	)
+	
+	# Set up focus navigation to letter buttons when pressing up
+	call_deferred("_setup_focus_navigation")
+
+func _setup_focus_navigation() -> void:
+	# Find the SelectLettersButtonContainer specifically
+	var scene_root = get_tree().current_scene
+	var letter_container = scene_root.find_child("SelectLettersButtonContainer", true, false)
+	
+	if letter_container:
+		# Find the first focusable letter button
+		for child in letter_container.get_children():
+			if child is Button and child.focus_mode != Control.FOCUS_NONE and not child.disabled:
+				set("focus_neighbor_up", child.get_path())
+				print("ResetButton: Setting focus_neighbor_up to: ", child.get_path())
+				break
 
 func _gui_input(event: InputEvent) -> void:
 	# Handle both "read" and "jump" actions to activate the reset button
