be08bf1ab6111febd40a1c3e1dfb999e3493f35c
Cody Romano <codyromano@gmail.com>
2025-10-28 19:09:26 -0700

    make the water dangerous

---
+++ scenes/OceanFloor.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+
+@onready var damage_area: Area2D = $OceanFloorDamageArea
+
+func _ready():
+	if damage_area:
+		damage_area.body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "ChonkiCharacter":
+		# Ocean floor contact removes all three hearts
+		GlobalSignals.player_hit.emit()
+		GlobalSignals.player_hit.emit()
+		GlobalSignals.player_hit.emit()
