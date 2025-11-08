976ddce8245ebb6baf4fdac0fc0201502a39db27
Cody Romano <codyromano@gmail.com>
2025-11-07 18:30:01 -0800

    isaac dialogue

--- scenes/composable/isaac_quest_giver.gd
+++ scenes/composable/isaac_quest_giver.gd
@@ -1,6 +1,7 @@
 extends "res://scenes/quest_giver.gd"
 
 var eagle_returned: bool = false
+var has_met_isaac: bool = false
 
 func _get_dialogue_tree() -> DialogueTree:
 	# Dialogue 2 - When Gus returns the eagle (completion dialogue) - Part 3
@@ -24,36 +25,44 @@ func _get_dialogue_tree() -> DialogueTree:
 	
 	# Dialogue 1 - First encounter (quest offer) - Part 4
 	var isaac_intro_part4 = DialogueNode.new()
-	isaac_intro_part4.text = "Look, I know 'air jail' sounds bad. But in my defense, he WAS throwing rocks at the mailman. I just… didn't think about the geese situation."
+	isaac_intro_part4.text = "Rodrigo's trapped in air jail. Please help me, Gus..."
 	isaac_intro_part4.choices = []
 	
 	# Dialogue 1 - First encounter (quest offer) - Part 3
 	var isaac_intro_part3 = DialogueNode.new()
-	isaac_intro_part3.text = "The clouds up there are overrun with angry geese. Every time I try to fly up, they chase me off. It's like they formed a union or something."
+	isaac_intro_part3.text = "But angry geese surrounded his timeout cloud."
 	isaac_intro_part3.choices = [
 		{"id": "isaac-continue-3", "text": "Continue", "next_node": isaac_intro_part4}
 	]
 	
 	# Dialogue 1 - First encounter (quest offer) - Part 2
 	var isaac_intro_part2 = DialogueNode.new()
-	isaac_intro_part2.text = "So here's the thing… I put my son in 'air jail' on a cloud platform up there. Yeah, I know. Parent of the year, right?"
+	isaac_intro_part2.text = "I put him in timeout for pooping on the cyclists."
 	isaac_intro_part2.choices = [
 		{"id": "isaac-continue-2", "text": "Continue", "next_node": isaac_intro_part3}
 	]
 	
 	# Dialogue 1 - First encounter (quest offer) - Part 1
 	var isaac_intro_part1 = DialogueNode.new()
-	isaac_intro_part1.text = "Hey! You look like someone who doesn't judge questionable parenting decisions. Can I level with you for a second?"
+	isaac_intro_part1.text = "My poor baby Rodrigo is trapped!"
 	isaac_intro_part1.choices = [
 		{"id": "isaac-continue-1", "text": "Continue", "next_node": isaac_intro_part2}
 	]
+	
+	# Repeat dialogue - when talking again before finding the eagle
+	var isaac_repeat = DialogueNode.new()
+	isaac_repeat.text = "Have you found my baby Rodrigo?"
+	isaac_repeat.choices = []
 
 	var isaac_tree = DialogueTree.new()
 	# Choose root based on whether eagle has been returned
 	if eagle_returned:
 		isaac_tree.root_node = isaac_thanks_part1
+	elif has_met_isaac:
+		isaac_tree.root_node = isaac_repeat
 	else:
 		isaac_tree.root_node = isaac_intro_part1
+		has_met_isaac = true
 	return isaac_tree
 	
 func get_next_dialogue_node_custom(current_node: DialogueNode, selected_option_id: String) -> DialogueNode:
